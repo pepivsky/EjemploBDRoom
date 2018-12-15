07ee738cf6cf235c7ffabe5fe7c0a3978e29bd50
Jose Manuel Paredes Ramirez <ironglas68@gmail.com>
2018-12-15 09:06:22 -0600

    Creación de la entidad profesorLenguaje para hacer la relación de muchos a muchos

--- app/src/main/java/com/example/pepe/ejemplobdroom/constants/Constants.java
+++ app/src/main/java/com/example/pepe/ejemplobdroom/constants/Constants.java
@@ -5,6 +5,7 @@ public class Constants {
     public static final String NAME_TABLE_PROFESSOR = "tprofessor";
     public static final String NAME_TABLE_CURSO = "tcurso";
     public static final String NAME_TABLE_LENGUAJE = "tlenguaje";
+    public static final String NAME_TABLE_PROFESSORLENGUAJE = "tprofessorlenguaje";
     public static final String NAME_DATABASE = "basedatos";
 
 }
---
+++ app/src/main/java/com/example/pepe/ejemplobdroom/db/entity/ProfessorLenguaje.java
@@ -0,0 +1,45 @@
+package com.example.pepe.ejemplobdroom.db.entity;
+
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.ForeignKey;
+
+import com.example.pepe.ejemplobdroom.constants.Constants;
+
+/**
+ * Entidad ProfesorLenguaje es una tabla intermedia entre professor y lenguaje para hacer la
+ * relación de muchos a muchos
+ * Un profesor puede enseñar muchos lenguajes de programacion y muchos lenguajes de programación pueden ser enseñados por muchos profesores
+ */
+
+@Entity(tableName = Constants.NAME_TABLE_PROFESSORLENGUAJE,
+primaryKeys = {"professorId", "lenguajeId"},
+foreignKeys =
+@ForeignKey(entity = TProfessor.class, parentColumns = "id", childColumns = "professorId"))
+@ForeignKey(entity = TLenguaje.class, parentColumns = "id", childColumns = "lenguajeId")
+
+public class ProfessorLenguaje {
+
+    @ColumnInfo(name = "professorId")
+    public int professorId;
+
+    @ColumnInfo(name = "lenguajeId")
+    public int lenguajeId;
+
+    public int getProfessorId() {
+        return professorId;
+    }
+
+    public void setProfessorId(int professorId) {
+        this.professorId = professorId;
+    }
+
+    public int getLenguajeId() {
+        return lenguajeId;
+    }
+
+    public void setLenguajeId(int lenguajeId) {
+        this.lenguajeId = lenguajeId;
+    }
+}
